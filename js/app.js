13fb741a86ea6558428bf037ec43c07007f62086
Jenny Brezuela <brezuelajenny@gmail.com>
2021-04-26 15:19:52 +0700

    kicking off the project

--- js/app.js
+++ js/app.js
@@ -1,70 +1,25 @@
-//Variables
-const send8tn = document.getElementById('send8tn'),
-email = document.getElementById('email'),
-subject = document.getElementById('subject'),
-message = document.getElementByI('message');
-
+//variables
+const sendBtn = document.getElementById('sendBtn'),
+      email = document.getElementById('email'),
+      message = document.getElementById('message'),
+      subject =document.getElementById('subject'),
+      resetBtn = document.getElementById('resetBtn'),
+      sendEmailForm = document.getElementById('email-form');
 
 //Event Listeners
 
-eventlisteners();
+eventListeners();
 
-function eventlisteners() {
-    //App Init
-    document.addEventListener('DOMContentLoaded', appInit);
+function eventListeners() {
+    //App init
+    document.addEventListener('DOMContentLoaded',appInit);
 
-    //validate the forms
-    email.addEventListener('blur',validateField);
-    subject.addEventListener('blur',validateField);
-    message.addEventListener('blur',validateField);
+    //Validate the form
+    email.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+
+    //Send email & reset Btn
+    sendEmailForm.addEventListener('submit', sendEmail);
+    resetBtn.addEventListener('click', resetForm);
 }
-
-//Functions
-
-//App Initialization
-function appInit() {
-    //disable the send button on load
-    send8tn.disable = true;
-}
-
-
-// validate the field
-function validateField(){
-let errors;
-
-//validate the Lenght of the field
-validateLenght(this);
-
-//validate the email
-if(this.type === 'email'){
-    validateEmail(this);
-}
-
-}
-//validate the lenght of the fields 
-function validateLenght(field){
-    if(field.value.lenght > 0 ){
-        field.style.boarderBottomColor = 'green';
-        field.classList.remove('error');
-    }else{
-        field.style.boarderBottomColor = 'red';
-        field.classList.add('error');
-    }
-
-}
-//validate email (checks for @ in the value )
-
-function validateEmail(field){
-    let emailText = field.value;
-    //check if the emailtext conatins the @ sign
-    if(emailText.index('@') !==-1){
-        field.style.boarderBottomColor = 'green';
-        field.classList.remove('error');
-    }else{
-        field.style.boarderBottomColor = 'red';
-        field.classList.add('error');
-    }
-
-    }
-
-

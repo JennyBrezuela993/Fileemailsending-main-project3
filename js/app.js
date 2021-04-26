06cdcb14ec7e823d556b82c1ec86f86f79200ba1
Jenny Brezuela <brezuelajenny@gmail.com>
2021-04-26 15:21:21 +0700

    validating field part 1

--- js/app.js
+++ js/app.js
@@ -23,3 +23,40 @@ function eventListeners() {
     sendEmailForm.addEventListener('submit', sendEmail);
     resetBtn.addEventListener('click', resetForm);
 }
+//Functions
+
+//App Initialization
+function appInit() {
+    //Disable the send Btn on load
+    sendBtn.disabled = true;
+}
+
+function sendEmail(e) {
+    e.preventDefault();
+
+    //Show the Spinner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'block';
+
+    //show the image
+    const sendEmailImg = document.createElement('img');
+    sendEmailImg.src = 'img/mail.gif';
+    sendEmailImg.style.display = 'block';
+
+    //Hide spinner then show the email image
+    setTimeout(function() {
+        //Hide the spinner
+        spinner.style.display = 'none';
+
+        //show the email image
+        document.querySelector('#loaders').appendChild(sendEmailImg);
+
+        //after 5 seconds hide the emailimage and reset the form
+        setTimeout(function() {
+            sendEmailForm.reset();
+            sendEmailImg.remove();
+            sendBtn.disabled = true;
+        }, 5000);
+
+    }, 3000 );
+}
